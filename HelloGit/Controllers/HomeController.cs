6823dcd5390470a6e892f94d20bc4c6310d5d3c8
Suchi <byiddish@gmail.com>
2016-03-28 12:41:05 -0400

    Initial Commit

---
+++ HelloGit/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloGit.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public void Index()
+        {
+            Response.Write("<h1>Hello github!!!<h1>");
+        }
+
+    }
+}
